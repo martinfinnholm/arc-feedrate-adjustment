c3d770185ad6735cda68ab71550cad4a6b2ee888
martinfinnholm <martin.finnholm@gmail.com>
2022-10-26 19:51:27 +0300

    remove abundant comments, clean code

--- arcFeedrateAdjust.py
+++ arcFeedrateAdjust.py
@@ -12,16 +12,12 @@ def user_input(prompt):
 print("Internal arc adjustment calculator\n")
 adjusted_feed = None
 while adjusted_feed is None:
-    """linear_feed = 211
-    major_dia = 14.27
-    cutter_dia = 9.398"""
 
     linear_feed = user_input("Linear feed: ")
     major_dia = user_input("Major diameter: ")
     cutter_dia = user_input("Cutter diameter: ")
-    try:
-        adjusted_feed = linear_feed * ((major_dia - cutter_dia) / major_dia)
-        print(("F" + str(round(adjusted_feed, 2))))
-    except ValueError:
-        print("Try again")
+
+    adjusted_feed = linear_feed * ((major_dia - cutter_dia) / major_dia)
+    print(("F" + str(round(adjusted_feed, 2))))
+
 
